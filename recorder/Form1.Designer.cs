44aeb5a68255bd93f6c3dd03c05818ecea977b67
Hirandalpe <hdalpatadu@gmail.com>
2020-10-17 19:30:21 +0530

    Recorder Initiallization

---
+++ recorder/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Capture = new System.Windows.Forms.Button();
+            this.timercapture = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(98, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(651, 341);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(475, 48);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(134, 52);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.Location = new System.Drawing.Point(615, 48);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(134, 52);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "Stop / Save";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Capture
+            // 
+            this.Capture.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Capture.Location = new System.Drawing.Point(98, 48);
+            this.Capture.Name = "Capture";
+            this.Capture.Size = new System.Drawing.Size(134, 52);
+            this.Capture.TabIndex = 3;
+            this.Capture.Text = "Capture";
+            this.Capture.UseVisualStyleBackColor = true;
+            this.Capture.Click += new System.EventHandler(this.Capture_Click);
+            // 
+            // timercapture
+            // 
+            this.timercapture.Tick += new System.EventHandler(this.timercapture_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 471);
+            this.Controls.Add(this.Capture);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Capture;
+        private System.Windows.Forms.Timer timercapture;
+    }
+}
+
---
+++ recorder/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace recorder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VideoFileWriter vw;
+        Bitmap bp;
+        Graphics gr;
+        string path = @"one"+DateTime.Now.ToString("ddssMM")+".mp4";
+        string foldername = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)+@"\record";
+                
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                vw = new VideoFileWriter();
+                System.IO.Directory.CreateDirectory(foldername);
+                vw.Open(foldername+@"\"+path, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, 10, VideoCodec.MPEG4, 100000);
+                timer1.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            gr = Graphics.FromImage(bp);
+            gr.CopyFromScreen(0, 0, 0, 0, bp.Size);
+            pictureBox1.Image = bp;
+            vw.WriteVideoFrame(bp);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1 = new Timer();
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            vw.Close();
+        }
+
+        private void Capture_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            timercapture.Start();
+        }
+
+        int i = 0;
+
+        private void timercapture_Tick(object sender, EventArgs e)
+        {
+            i++;
+            if (i >= 2)
+            {
+                i = 0;
+                Bitmap mp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics gr = Graphics.FromImage(mp);
+                gr.CopyFromScreen(0, 0, 0, 0, mp.Size);
+                pictureBox1.Image = mp;
+                pictureBox1.Image.Save(@"D:\shot.jpeg", ImageFormat.Jpeg);
+                this.WindowState = FormWindowState.Normal;
+                timercapture.Stop();
+            }
+        }
+    }
+}
